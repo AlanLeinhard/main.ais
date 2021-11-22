aef6417409c09f4028c8051dff20b066aa1a3f4f
VeeNDeN2233 <belenikin2048@mail.ru>
2021-11-22 16:32:56 +0300

    dfdf

--- app/static/AIS.js
+++
@@ -1,5 +0,0 @@
-
-
-function replaceContentInContainer(target,source) {
-    document.getElementById(target).innerHTML  = document.getElementById(source).innerHTML;
-  }
---
+++ app/static/js/AIS.js
@@ -0,0 +1,22 @@
+
+
+function replaceContentInContainer(target,source) {
+    document.getElementById(target).innerHTML  = document.getElementById(source).innerHTML;
+  }
+
+  function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
