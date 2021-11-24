73c0a2f64a63e2edcbe7a7568f8f1c61f7b121aa
AlanLeinhard <kommander.al@gmail.com>
2021-11-24 17:54:38 +0300

    add forms

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -1,19 +1,42 @@
 from wtforms import Form, BooleanField, StringField, PasswordField, validators
-from wtforms.fields.simple import FileField, SubmitField
+from wtforms.fields.simple import EmailField, FileField, SubmitField
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 from flask_wtf import FlaskForm
+
 
 class ServiceForm(FlaskForm):
     title = StringField('Название сервиса', [validators.Length(min=4, max=25)])
     desc = StringField('desc', [validators.Length(min=6, max=35)])
-    url_serv = StringField('url_serv',[validators.Length(min=6, max=35)])
+    url_serv = StringField('url_serv', [validators.Length(min=6, max=35)])
     image = FileField('image', validators=[
                       FileRequired(), FileAllowed(['jpg', 'jpeg', 'png', 'gif'])])
     submit = SubmitField("Добавить")
 
 
 class NewsForm(FlaskForm):
-    author_id = StringField("Заголовок новости", [validators.Length(min=4, max=25)])
+    author_id = StringField("Заголовок новости", [
+                            validators.Length(min=4, max=25)])
     title = StringField('Название сервиса', [validators.Length(min=4, max=25)])
     body = StringField('Название сервиса', [validators.Length(min=4, max=25)])
     submit = SubmitField("Добавить")
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Имя пользователя', [
+                           validators.Length(min=4, max=25)])
+    password = PasswordField('Новый пароль', [
+        validators.DataRequired()
+    ])
+    pass
+
+
+class RegisterForm(FlaskForm):
+    username = StringField('Имя пользователя', [
+                           validators.Length(min=4, max=25)])
+    email = StringField('Email-адрес', [validators.Length(min=6, max=35)])
+    password = PasswordField('Новый пароль', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Пароли должны совпадать')
+    ])
+    confirm = PasswordField('Повторите пароль')
+    pass
