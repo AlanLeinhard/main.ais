71f69eadb3bed51fbdf40f33b573cb026ad905b5
AlanLeinhard <kommander.al@gmail.com>
2021-11-27 15:12:47 +0300

    index image fix index post

--- app/site/views.py
+++ app/site/views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+import base64
 
 from app.models import Item, Post
 
@@ -8,6 +9,8 @@ bp = Blueprint("site", __name__)
 @bp.route("/")
 def index():
     """Show all the posts, most recent first."""
-    items = Item.query.order_by(Item.created).all()
     post = Post.query.order_by(Post.created).all()
-    return render_template("site/index.html", item=items, post=post)
+    items = Item.query.order_by(Item.created).all()
+    for el in items:
+        el.image = base64.b64encode(el.image).decode('ascii')
+    return render_template("site/index.html", data=list, item=items, post=post)
