84ed651c06d9811fd8d3a49014b30af57bc976dd
AlanLeinhard <kommander.al@gmail.com>
2021-11-28 10:01:37 +0300

    search done

--- app/site/forms.py
+++ app/site/forms.py
@@ -7,9 +7,9 @@ from flask_wtf.file import  FileRequired, FileAllowed
 
 
 class SearchForm(FlaskForm):
-    choices = [('google', 'google1'),
-               ('yandex', 'yandex1'),
-               ('Publisher', 'Publisher1')]
+    choices = [('google', 'Google'),
+               ('yandex', 'Yandex'),
+               ('duckduckgo', 'DuckDuckGo')]
     select = SelectField('Search:', choices=choices)
     search = StringField('')
     submit = SubmitField("Поиск")
--- app/site/views.py
+++ app/site/views.py
@@ -1,10 +1,10 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, url_for, redirect
 import base64
 
 from wtforms.form import Form
 
 from app.models import Item, Post
-from app.site.forms import  SearchForm
+from app.site.forms import SearchForm
 
 bp = Blueprint("site", __name__)
 
@@ -16,11 +16,6 @@ def index():
     post = Post.query.order_by(Post.created.desc()).all()
     item = Item.query.order_by(Item.created.desc()).all()
 
-    if form.validate_on_submit():
-        print("asdfdgf")
-        print(form.select.data)
-        print(form.search.data)
-
     for el in post:
         el.image = base64.b64encode(el.image).decode('ascii')
 
@@ -28,9 +23,18 @@ def index():
 
         el.image = base64.b64encode(el.image).decode('ascii')
 
-    
+    if form.validate_on_submit():
+        request = form.search.data
+        if form.select.data == "google":
+            return redirect("https://www.google.com/search?q="+request+"&sxsrf=AOaemvJV6dEIBTrjqBnthtVJRGnqAp7zVQ%3A1638082607415&source=hp&ei=LyijYbuGF8GprgTOupG4DQ&iflsig=ALs-wAMAAAAAYaM2P6alZ2L_AwgQ0amZuP4nctZ5n-Ai&ved=0ahUKEwj72bOfvbr0AhXBlIsKHU5dBNcQ4dUDCAg&uact=5&oq="+request+"&gs_lcp=Cgdnd3Mtd2l6EAMyBwgAELEDEEMyCAgAEIAEELEDMggIABCABBCxAzIICAAQgAQQsQMyCwgAEIAEELEDEIMBMggIABCABBCxAzIICAAQgAQQsQMyCAgAEIAEELEDMgUIABCABDIFCAAQgAQ6BAgAEAM6DgguEIAEELEDEMcBEKMCOg4ILhCABBCxAxDHARDRAzoRCC4QgAQQsQMQgwEQxwEQowI6BAgAEEM6EQguEIAEELEDEIMBEMcBENEDOgsILhCABBCxAxCDAToNCC4QsQMQxwEQ0QMQQzoHCAAQyQMQQzoFCAAQkgM6BAgAEA06BggAEA0QCjoNCC4QsQMQxwEQ0QMQDToKCC4QxwEQ0QMQDToHCCMQ6gIQJzoHCC4QsQMQQzoECC4QQ1AAWIYkYPUsaAFwAHgAgAHPAYgBkA-SAQYwLjEyLjGYAQCgAQGwAQU&sclient=gws-wiz")
+        elif form.select.data == "yandex":
+            return redirect("https://yandex.ru/yandsearch?clid=2028026&text="+request+"&lr=11373")
+        elif form.select.data == "duckduckgo":
+            return redirect("https://duckduckgo.com/?q="+request+"&t=h_&ia=web")
+        pass
 
     return render_template("site/index.html", data=list, item=item, post=post, form=form)
+
 
 @bp.route("/<int:id>")
 def news(id):
