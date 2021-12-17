d5e4e95c7537dc54f631854dd8d7e1ac9a1b9ee8
AlanLeinhard <kommander.al@gmail.com>
2021-12-17 16:02:20 +0300

    Update db_create.py

--- db_create.py
+++ db_create.py
@@ -6,13 +6,13 @@ import psycopg2
 from psycopg2 import sql
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT # <-- ADD THIS LINE
 
-# con = psycopg2.connect(dbname='postgres',
-#       user='postgres', host='172.19.0.2',
-#       password='postgres')
+con = psycopg2.connect(dbname='postgres',
+      user='postgres', host='172.19.0.2',
+      password='postgres')
 
-db.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT) # <-- ADD THIS LINE
+con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT) # <-- ADD THIS LINE
 
-cur = db.cursor()
+cur = con.cursor()
 
 # Use the psycopg2.sql module instead of string concatenation 
 # in order to avoid sql injection attacs.
